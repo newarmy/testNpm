4663b0eeac5f0ed380936207f7755cee7bb4ffc1
xinjundong <xinjundong@sohu-inc.com>
2020-04-21 15:53:15 +0800

    add code

---
+++ index.js
@@ -0,0 +1,9 @@
+import {add, reduce} from './src/math.js'
+import {concatString, split} from './src/math.js'
+
+export {
+	add,
+	reduce,
+	concatString,
+	split
+};
---
+++ src/string.js
@@ -0,0 +1,7 @@
+export function concatString(x,y) {
+	return x + y;
+}
+
+export function split(x, y) {
+	return x.split(y);
+}
